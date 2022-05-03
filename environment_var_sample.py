5af4b9ba454f285e1266525d97f06dcd582dc69f
cgmowl <52333507+cgmowl@users.noreply.github.com>
2022-05-03 11:23:23 -0400

    Create environment_var_sample.py

---
+++ environment_var_sample.py
@@ -0,0 +1,10 @@
+#Variables for the Production Instance of WebkeyServerCloud
+
+secret_arn = 'SECRET_ARN'
+secret_region = 'us-east-2'
+dynamo_table_east1 = 'arn:aws:dynamodb:us-east-1:123456789012:table/<DYNAMO_ARN>'
+dynamo_table_east2 = 'arn:aws:dynamodb:us-east-2:123456789012:table/<DYNAMO_ARN>'
+dynamo_host = '<DYNAMO_TABLE_NAME>'
+launch_template = 'lt-<NUMBER>'
+auto_scaling_group = '<ASG_NAME>'
+region='us-east-2'
